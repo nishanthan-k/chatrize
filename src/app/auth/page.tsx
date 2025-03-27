e839baf3100928156db91de276d6c8f4f7ab879d
nishanthan-k <krn.nishanthan@gmail.com>
2025-03-27 20:56:09 +0530

    feat: auth page

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,23 +1,46 @@
-"use client";
+"use client"
 
 import Github from '@/components/icons/Github';
 import Google from '@/components/icons/Google';
-import React from 'react'
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { FieldValues, useForm } from 'react-hook-form';
 
-function page() {
+// interface FormData {
+//   email: string;
+//   password: string;
+// }
+
+const Page = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm()
+
+  const onSubmit = (data: FieldValues) => console.log(data);
+
   return (
-    <main className='flex flex-col justify-center items-center'>
-      Signin / Signup
-      <section className='flex gap-2'>
-        <button className='flex gap-2'>
+    <main className='flex flex-col items-center justify-center gap-4 h-screen w-screen p-4'>
+      <h1>Welcome to Chatrize</h1>
+      <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-2 max-w-sm w-full'>
+        <Input {...register('email',  { required: true })} placeholder='Email' />
+        {errors.email && <span className='text-red-500'>Email is required</span>}
+
+        <Input {...register('password',  { required: true })} placeholder='Password' />
+        {errors.password && <span className='text-red-500'>Password is required</span>}
+        
+        <Button type='submit'>Submit</Button>
+      </form>
+      <section className='flex flex-col items-center gap-2'>
+        <p>Or continue with</p>
+        <div className='flex gap-2'>
+          <Button>
           <Github /> Github
-        </button>
-        <button className='flex gap-2'>
+        </Button>
+        <Button>
           <Google /> Google
-        </button>
+        </Button>
+        </div>
       </section>
     </main>
   )
 }
 
-export default page
+export default Page;
